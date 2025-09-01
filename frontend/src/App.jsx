a68643d85e0937f41ac6cd8f6103ddd152d273f7
ANUBOTHU ARAVIND <2200080137@kluniversity.in>
2025-09-01 10:48:03 +0000

    add .gitignore for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '2rem' }}>
+      <h1>📸 Photo Invoice App</h1>
+      <p>Your React + Vite frontend is running!</p>
+    </div>
+  )
+}
+
+export default App
